f287d057453b31f58406be7cea704d399f951bf6
ArslanQureshi <dexter8969@gmail.com>
2020-06-15 16:48:31 +0500

    update app.js with for all

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World from A.A Coders.
+          Hello World from A.A Coders for all.
         </p>
         <a
           className="App-link"
