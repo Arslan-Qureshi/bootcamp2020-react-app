60a931feb0eefd37231363dba1b9e87e1c412670
ArslanQureshi <dexter8969@gmail.com>
2020-06-25 10:54:01 +0500

    app.js update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World from A.A Coders from ahmad and arslan.
+          Hello World from A.A Coders from ahmad.
         </p>
         <a
           className="App-link"
---
+++ src/dinner.js
@@ -0,0 +1,4 @@
+import React from 'react';
+function Dinner(){
+    return <h1>hello world</h1>
+}
