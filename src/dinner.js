44237b584e85cf6547371d0952acd1f58f501971
ArslanQureshi <dexter8969@gmail.com>
2020-06-25 18:12:44 +0500

    update  app.js and add dinner.js

--- src/dinner.js
+++ src/dinner.js
@@ -1,4 +1,9 @@
 import React from 'react';
-function Dinner(){
-    return <h1>hello world</h1>
+function Dinner(props) {
+    return (
+        <div>
+            <h1>i m trying to learn {props.courseName}</h1>
+        </div>
+    );
 }
+export default Dinner;
